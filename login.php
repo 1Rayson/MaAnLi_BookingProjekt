df8602ba1422f3edaaf424a88dfc49020f4e70cb
1Rayson <andersskrydstrup@gmail.com>
2022-12-06 11:38:33 +0100

    Backend coding and more pages added

--- login.php
+++ login.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style-anders.css">
     <title>login</title>
 </head>
-    <body id="login-body">
-        <section id="login-section">
+    <body>
+        <section class="login--box">
             <form action="backend.php?action=login" method="post" id="login-form">
                 <?php 
                     if($fail)echo "<p id='log-in-fail-text'>Your password or username is wrong</p>";
