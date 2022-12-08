8843b3df3cbef812d7a44420a0557fae580aabe8
MarBProduction <MarBProduction@users.noreply.github.com>
2022-12-08 14:02:01 +0100

    Started to work on display availability

--- floorplan.js
+++ floorplan.js
@@ -14,3 +14,85 @@ function switchFloor(floor){
     }
     floorBtns[floor].className += " active-floor-btns";
 }
+
+let bookings = [
+    {
+        fullName: "Test subject1",
+        roomNumber: "30",
+        floorVariable: "s",
+        start_time: "2022-12-08",
+        end_time: ""
+    },
+    {
+        fullName: "Test subject2",
+        roomNumber: "01",
+        floorVariable: "1"
+    },
+    {
+        fullName: "Test subject3",
+        roomNumber: "17",
+        floorVariable: "1"
+    },
+    {
+        fullName: "Test subject4",
+        roomNumber: "24",
+        floorVariable: "1"
+    },
+    {
+        fullName: "Test subject15",
+        roomNumber: "44",
+        floorVariable: "1"
+    },
+    {
+        fullName: "Test subject6",
+        roomNumber: "11",
+        floorVariable: "2"
+    },
+    {
+        fullName: "Test subject7",
+        roomNumber: "20",
+        floorVariable: "2"
+    },
+    {
+        fullName: "Test subject8",
+        roomNumber: "26",
+        floorVariable: "2"
+    },
+    {
+        fullName: "Test subject9",
+        roomNumber: "38",
+        floorVariable: "2"
+    },
+];
+
+let rooms = document.querySelectorAll('.room');
+
+rooms.forEach(room => {
+    room.className.replace(" unavailable", "");
+    room.className.replace(" available", "");
+});
+
+rooms.forEach(room => {
+    for(i=0; i<bookings.length; i++){
+        if(room.id == "r" + bookings[i].floorVariable + bookings[i].roomNumber){
+            room.className += " unavailable";
+
+            const d = new Date("1922-03-25");
+        } else {
+            room.className += " available";
+        }
+    }
+});
+
+/* Til delvist ledigt, kan bruge MySQL query: 
+ligger VALGT start tid mellem BOOKET start og slut tid, 
+eller ligger VALGT slut tid mellem BOOKET start og slut tid. */
+
+/* Kan starte med at lave en liste over alle rum, som er booket bare et minut i valgt tidsrum.
+Dem der ikke er på den liste gemmer jeg i et array, og looper igennem arrayet for at tilføje styling.
+Resten kan jeg tjekke for om VALGT start tiden ligger før BOOKET start tid og om VALGT slut tid ligger efter BOOKET slut tid,
+i så fald vil det være "unavailable" ellers vil det være "partly-available" */
+
+/* Bliver nok nød til at omregne dato til antal millisekunder siden 1970, og så samligne det på en eller anden måde */
+
+/* Eller kan man med MySQL query tjekke om hver booking ligger indenfor valgt start og slut tidspunkt? */
--- floorplan.php
+++ floorplan.php
@@ -12,11 +12,11 @@
         <div id="fixed-ratio">
             <div id="grid-wrapper">
                 <div id="floor-s-grid">
-                    <div id="rS30">S.30</div>
-                    <div id="rS31">S.31</div>
-                    <div id="wcS1" class="unavailable">WC and stairs</div>
-                    <div id="wcS2" class="unavailable">WC and elevator</div>
-                    <div id="wcS4" class="unavailable">WC and stairs</div>
+                    <div id="rs30" class="room">S.30</div>
+                    <div id="rs31" class="room">S.31</div>
+                    <div id="wcs1" class="unavailable">WC and stairs</div>
+                    <div id="wcs2" class="unavailable">WC and elevator</div>
+                    <div id="wcs4" class="unavailable">WC and stairs</div>
                     <div id="administration-area-1" class="unavailable"></div>
                     <div id="administration-area-2" class="unavailable"></div>
                     <div id="administration-area-3" class="unavailable"></div>
@@ -26,52 +26,52 @@
                     <div id="holeS"></div>
                 </div>
                 <div id="floor-1-grid">
-                    <div id="r101" class="partly-available">1.01</div>
-                    <div id="r108" class="unavailable">1.08</div>
+                    <div id="r101" class="room">1.01</div>
+                    <div id="r108" class="room">1.08</div>
                     <div id="wc11" class="unavailable">WC and stairs</div>
-                    <div id="r112" class="unavailable">1.12</div>
+                    <div id="r112" class="room">1.12</div>
                     <div id="wc12" class="unavailable">WC and elevator</div>
-                    <div id="r116" class="available">1.16</div>
-                    <div id="r117" class="partly-available">1.17</div>
-                    <div id="r118" class="partly-available">1.18</div>
-                    <div id="r119">1.19</div>
-                    <div id="r124">1.24</div>
+                    <div id="r116" class="room">1.16</div>
+                    <div id="r117" class="room">1.17</div>
+                    <div id="r118" class="room">1.18</div>
+                    <div id="r119" class="room">1.19</div>
+                    <div id="r124" class="room">1.24</div>
                     <div id="wc13" class="unavailable">WC and stairs</div>
-                    <div id="r127">1.27</div>
+                    <div id="r127" class="room">1.27</div>
                     <div id="printer1" class="unavailable">Printer</div>
-                    <div id="r128">1.28</div>
-                    <div id="r132">1.32</div>
+                    <div id="r128" class="room">1.28</div>
+                    <div id="r132" class="room">1.32</div>
                     <div id="wc14" class="unavailable">WC and stairs</div>
-                    <div id="r138">1.38</div>
-                    <div id="r139">1.39</div>
-                    <div id="r140">1.40</div>
-                    <div id="r142">1.42</div>
-                    <div id="r143">1.43</div>
-                    <div id="r144">1.44</div>
+                    <div id="r138" class="room">1.38</div>
+                    <div id="r139" class="room">1.39</div>
+                    <div id="r140" class="room">1.40</div>
+                    <div id="r142" class="room">1.42</div>
+                    <div id="r143" class="room">1.43</div>
+                    <div id="r144" class="room">1.44</div>
                     <div id="hole1"></div>
                 </div>
                 <div id="floor-2-grid">
-                    <div id="r201">2.01</div>
+                    <div id="r201" class="room">2.01</div>
                     <div id="wc21" class="unavailable">WC and stairs</div>
-                    <div id="r207">2.07</div>
-                    <div id="r209">2.09</div>
+                    <div id="r207" class="room">2.07</div>
+                    <div id="r209" class="room">2.09</div>
                     <div id="service-desk" class="unavailable">Sevice Desk</div>
-                    <div id="r211">2.11</div>
+                    <div id="r211" class="room">2.11</div>
                     <div id="wc22" class="unavailable">WC and elevator</div>
-                    <div id="r216">2.16</div>
-                    <div id="r217">2.17</div>
-                    <div id="r218">2.18</div>
-                    <div id="r220">2.20</div>
-                    <div id="r221">2.21</div>
-                    <div id="r222">2.22</div>
+                    <div id="r216" class="room">2.16</div>
+                    <div id="r217" class="room">2.17</div>
+                    <div id="r218" class="room">2.18</div>
+                    <div id="r220" class="room">2.20</div>
+                    <div id="r221" class="room">2.21</div>
+                    <div id="r222" class="room">2.22</div>
                     <div id="wc23" class="unavailable">WC and stairs</div>
-                    <div id="r225">2.25</div>
-                    <div id="r226">2.26</div>
-                    <div id="r231">2.31</div>
+                    <div id="r225" class="room">2.25</div>
+                    <div id="r226" class="room">2.26</div>
+                    <div id="r231" class="room">2.31</div>
                     <div id="wc24" class="unavailable">WC and stairs</div>
-                    <div id="r237">2.37</div>
-                    <div id="r238">2.38</div>
-                    <div id="r241">2.41</div>
+                    <div id="r237" class="room">2.37</div>
+                    <div id="r238" class="room">2.38</div>
+                    <div id="r241" class="room">2.41</div>
                     <div id="hole2"></div>
                 </div>
             </div>
