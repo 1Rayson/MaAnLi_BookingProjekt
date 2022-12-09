0712531964bf288e9a28077bbb55effd69b113e2
MarBProduction <MarBProduction@users.noreply.github.com>
2022-12-09 13:57:08 +0100

    Mangler kun kommunikation med database i floorplan

--- floorplan.js
+++ floorplan.js
@@ -15,7 +15,7 @@ function switchFloor(floor){
     floorBtns[floor].className += " active-floor-btns";
 }
 
-let bookings = [
+let unavailableRooms = [
     {
         fullName: "Test subject1",
         roomNumber: "30",
@@ -24,18 +24,8 @@ let bookings = [
         end_time: ""
     },
     {
-        fullName: "Test subject2",
-        roomNumber: "01",
-        floorVariable: "1"
-    },
-    {
         fullName: "Test subject3",
         roomNumber: "17",
-        floorVariable: "1"
-    },
-    {
-        fullName: "Test subject4",
-        roomNumber: "24",
         floorVariable: "1"
     },
     {
@@ -44,18 +34,8 @@ let bookings = [
         floorVariable: "1"
     },
     {
-        fullName: "Test subject6",
-        roomNumber: "11",
-        floorVariable: "2"
-    },
-    {
         fullName: "Test subject7",
         roomNumber: "20",
-        floorVariable: "2"
-    },
-    {
-        fullName: "Test subject8",
-        roomNumber: "26",
         floorVariable: "2"
     },
     {
@@ -65,34 +45,59 @@ let bookings = [
     },
 ];
 
+let partlyAvailableRooms = [
+    {
+        fullName: "Test subject2",
+        roomNumber: "01",
+        floorVariable: "1"
+    },
+    {
+        fullName: "Test subject4",
+        roomNumber: "24",
+        floorVariable: "1"
+    },
+    {
+        fullName: "Test subject6",
+        roomNumber: "11",
+        floorVariable: "2"
+    },
+    {
+        fullName: "Test subject8",
+        roomNumber: "26",
+        floorVariable: "2"
+    }
+]
+
 let rooms = document.querySelectorAll('.room');
 
 rooms.forEach(room => {
     room.className.replace(" unavailable", "");
+    room.className.replace(" partly-available", "");
     room.className.replace(" available", "");
 });
 
 rooms.forEach(room => {
-    for(i=0; i<bookings.length; i++){
-        if(room.id == "r" + bookings[i].floorVariable + bookings[i].roomNumber){
+    for(i=0; i<unavailableRooms.length; i++){
+        if(room.id == "r" + unavailableRooms[i].floorVariable + unavailableRooms[i].roomNumber){
             room.className += " unavailable";
-
-            const d = new Date("1922-03-25");
-        } else {
-            room.className += " available";
+            return
         }
     }
+
+    for(i=0; i<partlyAvailableRooms.length; i++){
+        if (room.id == "r" + partlyAvailableRooms[i].floorVariable + partlyAvailableRooms[i].roomNumber){
+            room.className += " partly-available";
+            room.addEventListener("click", function() {selectRoom(room.id)});
+            return
+        }
+    }
+
+    room.className += " available";
+    room.addEventListener("click", function() {selectRoom(room.id)});
 });
 
-/* Til delvist ledigt, kan bruge MySQL query: 
-ligger VALGT start tid mellem BOOKET start og slut tid, 
-eller ligger VALGT slut tid mellem BOOKET start og slut tid. */
+function selectRoom(roomID){
+    console.log(roomID);
+}
 
-/* Kan starte med at lave en liste over alle rum, som er booket bare et minut i valgt tidsrum.
-Dem der ikke er på den liste gemmer jeg i et array, og looper igennem arrayet for at tilføje styling.
-Resten kan jeg tjekke for om VALGT start tiden ligger før BOOKET start tid og om VALGT slut tid ligger efter BOOKET slut tid,
-i så fald vil det være "unavailable" ellers vil det være "partly-available" */
-
-/* Bliver nok nød til at omregne dato til antal millisekunder siden 1970, og så samligne det på en eller anden måde */
-
-/* Eller kan man med MySQL query tjekke om hver booking ligger indenfor valgt start og slut tidspunkt? */
+/* kan sende via url, eller som "include" i body så vil det være POST i stedet. */
