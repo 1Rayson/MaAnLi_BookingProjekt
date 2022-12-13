a2833c2e05998574ebb9954392df6ad71f2a5b46
1Rayson <andersskrydstrup@gmail.com>
2022-12-13 17:18:38 +0100

    ja da, så er rummene opdateret

--- room_availability.php
+++ room_availability.php
@@ -137,51 +137,51 @@
                                 </div>
                                 <div id="floor-1-grid">
                                     <div id="101" class="room r101">1.01</div>
-                                    <div id="r108" class="room">1.08</div>
+                                    <div id="108" class="room r108">1.08</div>
                                     <div id="wc11" class="unavailable">WC and stairs</div>
-                                    <div id="r112 112" class="room">1.12</div>
+                                    <div id="112" class="room r112">1.12</div>
                                     <div id="wc12" class="unavailable">WC and elevator</div>
-                                    <div id="r116" class="room">1.16</div>
-                                    <div id="r117" class="room">1.17</div>
-                                    <div id="r118" class="room">1.18</div>
-                                    <div id="r119" class="room">1.19</div>
-                                    <div id="r124" class="room">1.24</div>
+                                    <div id="116" class="room r116">1.16</div>
+                                    <div id="117" class="room r117">1.17</div>
+                                    <div id="118" class="room r118">1.18</div>
+                                    <div id="119" class="room r119">1.19</div>
+                                    <div id="124" class="room r124">1.24</div>
                                     <div id="wc13" class="unavailable">WC and stairs</div>
-                                    <div id="r127" class="room">1.27</div>
+                                    <div id="127" class="room r127">1.27</div>
                                     <div id="printer1" class="unavailable">Printer</div>
-                                    <div id="r128" class="room">1.28</div>
-                                    <div id="r132" class="room">1.32</div>
+                                    <div id="128" class="room r128">1.28</div>
+                                    <div id="132" class="room r132">1.32</div>
                                     <div id="wc14" class="unavailable">WC and stairs</div>
-                                    <div id="r138" class="room">1.38</div>
-                                    <div id="r139" class="room">1.39</div>
-                                    <div id="r140" class="room">1.40</div>
-                                    <div id="r142" class="room">1.42</div>
-                                    <div id="r143" class="room">1.43</div>
-                                    <div id="r144" class="room">1.44</div>
+                                    <div id="138" class="room r138">1.38</div>
+                                    <div id="139" class="room r139">1.39</div>
+                                    <div id="140" class="room r140">1.40</div>
+                                    <div id="142" class="room r142">1.42</div>
+                                    <div id="143" class="room r143">1.43</div>
+                                    <div id="144" class="room r144">1.44</div>
                                     <div id="hole1"></div>
                                 </div>
                                 <div id="floor-2-grid">
-                                    <div id="r201" class="room">2.01</div>
+                                    <div id="201" class="room r201">2.01</div>
                                     <div id="wc21" class="unavailable">WC and stairs</div>
-                                    <div id="r207" class="room">2.07</div>
-                                    <div id="r209" class="room">2.09</div>
+                                    <div id="207" class="room r207">2.07</div>
+                                    <div id="209" class="room r209">2.09</div>
                                     <div id="service-desk" class="unavailable">Sevice Desk</div>
-                                    <div id="r211" class="room">2.11</div>
+                                    <div id="211" class="room r211">2.11</div>
                                     <div id="wc22" class="unavailable">WC and elevator</div>
-                                    <div id="r216" class="room">2.16</div>
-                                    <div id="r217" class="room">2.17</div>
-                                    <div id="r218" class="room">2.18</div>
-                                    <div id="r220" class="room">2.20</div>
-                                    <div id="r221" class="room">2.21</div>
-                                    <div id="r222" class="room">2.22</div>
+                                    <div id="216" class="room r216">2.16</div>
+                                    <div id="217" class="room r217">2.17</div>
+                                    <div id="218" class="room r218">2.18</div>
+                                    <div id="220" class="room r220">2.20</div>
+                                    <div id="221" class="room r221">2.21</div>
+                                    <div id="222" class="room r222">2.22</div>
                                     <div id="wc23" class="unavailable">WC and stairs</div>
-                                    <div id="r225" class="room">2.25</div>
-                                    <div id="r226" class="room">2.26</div>
-                                    <div id="r231" class="room">2.31</div>
+                                    <div id="225" class="room r225">2.25</div>
+                                    <div id="226" class="room r226">2.26</div>
+                                    <div id="231" class="room r231">2.31</div>
                                     <div id="wc24" class="unavailable">WC and stairs</div>
-                                    <div id="r237" class="room">2.37</div>
-                                    <div id="r238" class="room">2.38</div>
-                                    <div id="r241" class="room">2.41</div>
+                                    <div id="237" class="room r237">2.37</div>
+                                    <div id="238" class="room r238">2.38</div>
+                                    <div id="241" class="room r241">2.41</div>
                                     <div id="hole2"></div>
                                 </div>
                             </div>
