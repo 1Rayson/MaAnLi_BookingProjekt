4184d8f8f7a680a114d9ebc818194a6d33e14c9a
MarBProduction <MarBProduction@users.noreply.github.com>
2022-12-14 18:07:51 +0100

    styled kalender og tids select page

--- book_lokale.php
+++ book_lokale.php
@@ -34,7 +34,7 @@
                 <h2 id="breadcrumb-nav-mobile">Book Lokale</h2>
             </nav>
 
-            <article class="content">
+            <article class="content" onchange="activateButton()">
                 <section id="calendar-container">
                     <h2>Vælg dag</h2>
                     <?php
@@ -46,75 +46,79 @@
                 <section id="time-container">
                     <h2>Vælg tidsrum</h2>
                     <form method="post">
-                        <label for="start_hour">Fra:</label>
-                        <select id="start_hour" name="start_hour">
-                        <option value="01">1</option>
-                        <option value="02">2</option>
-                        <option value="03">3</option>
-                        <option value="04">4</option>
-                        <option value="05">5</option>
-                        <option value="06">6</option>
-                        <option value="07">7</option>
-                        <option selected="selected" value="08">8</option>
-                        <option value="09">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        </select>
-                        <p>:</p>
-                        <select id="start_minute" name="start_minute">
-                        <option value="00">00</option>
-                        <option value="15">15</option>
-                        <option value="30">30</option>
-                        <option value="45">45</option>
-                        </select>
-                        <label for="end_time">Til:</label>
-                        <select id="end_hour" name="end_hour">
-                        <option value="01">1</option>
-                        <option value="02">2</option>
-                        <option value="03">3</option>
-                        <option value="04">4</option>
-                        <option value="05">5</option>
-                        <option value="06">6</option>
-                        <option value="08">8</option>
-                        <option value="09">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option selected="selected" value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        </select>
-                        <p>:</p>
-                        <select id="end_minute" name="end_minute">
-                        <option value="00">00</option>
-                        <option value="15">15</option>
-                        <option value="30">30</option>
-                        <option value="45">45</option>
-                        </select>
+                        <div class="time-picker-flex">
+                            <label for="start_hour">Fra:</label>
+                            <select id="start_hour" name="start_hour">
+                                <option value="01">1</option>
+                                <option value="02">2</option>
+                                <option value="03">3</option>
+                                <option value="04">4</option>
+                                <option value="05">5</option>
+                                <option value="06">6</option>
+                                <option value="07">7</option>
+                                <option selected="selected" value="08">8</option>
+                                <option value="09">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                                <option value="15">15</option>
+                                <option value="16">16</option>
+                                <option value="17">17</option>
+                                <option value="18">18</option>
+                                <option value="19">19</option>
+                                <option value="20">20</option>
+                                <option value="21">21</option>
+                                <option value="22">22</option>
+                                <option value="23">23</option>
+                                <option value="24">24</option>
+                            </select>
+                            <p>:</p>
+                            <select id="start_minute" name="start_minute">
+                                <option value="00">00</option>
+                                <option value="15">15</option>
+                                <option value="30">30</option>
+                                <option value="45">45</option>
+                            </select>
+                        </div>
+                        <div class="time-picker-flex">
+                            <label for="end_time">Til:</label>
+                            <select id="end_hour" name="end_hour">
+                                <option value="01">1</option>
+                                <option value="02">2</option>
+                                <option value="03">3</option>
+                                <option value="04">4</option>
+                                <option value="05">5</option>
+                                <option value="06">6</option>
+                                <option value="08">8</option>
+                                <option value="09">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option selected="selected" value="14">14</option>
+                                <option value="15">15</option>
+                                <option value="16">16</option>
+                                <option value="17">17</option>
+                                <option value="18">18</option>
+                                <option value="19">19</option>
+                                <option value="20">20</option>
+                                <option value="21">21</option>
+                                <option value="22">22</option>
+                                <option value="23">23</option>
+                                <option value="24">24</option>
+                            </select>
+                            <p>:</p>
+                            <select id="end_minute" name="end_minute">
+                                <option value="00">00</option>
+                                <option value="15">15</option>
+                                <option value="30">30</option>
+                                <option value="45">45</option>
+                            </select>
+                        </div>
                     </form>
-                    <button id="submit-button" onclick="collectInfo()">Tjek ledighed</button>
+                    <button id="submit-button">Tjek ledighed</button>
                 </section>
 
 
@@ -177,19 +181,47 @@
     <script src="calendar.js"></script>
     <script>
 
+        let seeAvailButton = document.getElementById('submit-button');
+        let date = document.getElementsByClassName('activeDate');
+        let startHour = document.getElementById('start_hour').value;
+        let startMinute = document.getElementById('start_minute').value;
+        let endHour = document.getElementById('end_hour').value;
+        let endMinute = document.getElementById('end_minute').value;
+
+        seeAvailButton.classList.add("button-deactivated");
+
         function collectInfo(){
-            let date = document.getElementsByClassName('activeDate');
-            let startHour = document.getElementById('start_hour').value;
-            let startMinute = document.getElementById('start_minute').value;
-            let endHour = document.getElementById('end_hour').value;
-            let endMinute = document.getElementById('end_minute').value;
-            
+            console.log("collecting info");       
             window.location.href='room_availability.php?date='+ date[0].id
                 +'&start-hour='+ startHour
                 +'&start-minute='+ startMinute
                 +'&end-hour='+ endHour
                 +'&end-minute='+ endMinute
             ;
+        }
+
+        function activateButton (){
+            let date = document.getElementsByClassName('activeDate');
+            let startHour = document.getElementById('start_hour').value;
+            let startMinute = document.getElementById('start_minute').value;
+            let endHour = document.getElementById('end_hour').value;
+            let endMinute = document.getElementById('end_minute').value;
+
+            console.log("activate button?");
+
+            if(date[0]!=null && startHour!="" && startMinute!="" && endHour!="" && endMinute!=""){
+                seeAvailButton.classList.remove("button-deactivated");
+                seeAvailButton.classList.add("button-activated");
+
+                seeAvailButton.addEventListener("click", function(){collectInfo()});
+                console.log("button active");
+            } else {
+                seeAvailButton.classList.remove("button-activated");
+                seeAvailButton.classList.add("button-deactivated");
+
+                seeAvailButton.removeEventListener("click", function(){collectInfo()});
+                console.log("button deactive");
+            }
         }
 
     </script>
--- calendar.js
+++ calendar.js
@@ -15,13 +15,12 @@ function toggleSelected(elem) {
       for (var i = 0; i < elem.length; i++) {
         if (current != elem[i]) {
           elem[i].classList.remove("activeDate");
-        } else if (current.classList.contains("activeDate") === true) {
-          current.classList.remove("activeDate");
         } else {
           current.classList.add("activeDate");
         }
       }
       e.preventDefault();
+      activateButton ();
     });
   }
 }
